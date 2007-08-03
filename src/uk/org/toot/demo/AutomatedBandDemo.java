602dadf7bc7ccb15c5d4b7f8cf57df1445dac4f4
TootSoftware <TootSoftware@0bbddd95-922e-0410-a663-c16033ac15ba>
2007-08-03 14:30:22 +0000

    Update AuomatedBandDemo according to latest composition refactoring.

--- src/uk/org/toot/demo/AutomatedBandDemo.java
+++ src/uk/org/toot/demo/AutomatedBandDemo.java
@@ -8,6 +8,7 @@ package uk.org.toot.demo;
 import uk.org.toot.music.composition.*;
 import uk.org.toot.tonality.Scales;
 import java.io.File;
+import static uk.org.toot.midi.misc.GM.*;
 
 public class AutomatedBandDemo 
 {
@@ -22,29 +23,47 @@ public class AutomatedBandDemo
 		band.setCycleOfFifthsDensity(0.33f);
 		band.setMaxKeys(5);
 		
-		DrumKitComposer kit = new DrumKitComposer("Drums", 0, 9);
-		band.addComposer(kit);
+		addDrumKitComposer(0, 9);
+		addBassComposer(35, 0); // fretless bass
+		addKeyboardComposer(0, 2); // acoustic grand piano
+	}
+
+	protected void addDrumKitComposer(int program, int channel) {
+		RhythmicComposer kickComposer = new RhythmicComposer("Kick", program, channel);
+		band.addComposer(kickComposer);
+		RhythmicComposer leftHandComposer = new RhythmicComposer("Drums Left", program, channel);
+		band.addComposer(leftHandComposer);
+		RhythmicComposer rightHandComposer = new RhythmicComposer("Drums Right", program, channel);
+		band.addComposer(rightHandComposer);
 		
-		RhythmicComposer.Context ridmContext = new RhythmicComposer.Context();
+		RhythmicComposer.Context ridmContext = new RhythmicComposer.SingleDrumContext(ACOUSTIC_BASS_DRUM);
 		ridmContext.setDensity(0.60f);
 		ridmContext.setMinNoteLen(8);
 		ridmContext.setJamTiming(Timing.ODD_DOWNBEATS);
-		kit.getKickComposer().setContext(ridmContext);
+		kickComposer.setContext(ridmContext);
+		kickComposer.setSwingRatio(1.1f);
 		
-		ridmContext = new RhythmicComposer.Context();
+		ridmContext = new RhythmicComposer.SingleDrumContext(ACOUSTIC_SNARE);
 		ridmContext.setDensity(0.40f);
 		ridmContext.setMinNoteLen(16);
 		ridmContext.setJamTiming(Timing.EVEN_DOWNBEATS);
 		ridmContext.setClearTiming(Timing.ODD_DOWNBEATS);
-		kit.getLeftHandComposer().setContext(ridmContext);
+		leftHandComposer.setContext(ridmContext);
+		leftHandComposer.setSwingRatio(1.1f);
 		
-		ridmContext.setDensity(0.50f);
+		ridmContext = new RhythmicComposer.DualDrumContext(
+				CLOSED_HI_HAT, 0.95f, OPEN_HI_HAT);
+		ridmContext.setDensity(0.80f);
 		ridmContext.setMinNoteLen(16);
 		ridmContext.setJamTiming(Timing.ALL_UPBEATS);
-		ridmContext.setClearTiming(1);
-		kit.getRightHandComposer().setContext(ridmContext);
-		
-		TonalComposer bass = new TonalComposer("Bass", 35, 0); // fretless bass
+		ridmContext.setAccent(-24);
+		ridmContext.setAccentTiming(Timing.ALL_UPBEATS);
+		rightHandComposer.setContext(ridmContext);
+		rightHandComposer.setSwingRatio(1.1f);
+	}
+
+	protected void addBassComposer(int program, int channel) {
+		TonalComposer bass = new TonalComposer("Bass", program, channel); // fretless bass
 		TonalComposer.Context bassContext = new TonalComposer.Context();
 		bassContext.setMelodyProbability(1.0f); // 100% melodic
 		bassContext.setRepeatPitchProbability(0.25f);
@@ -58,10 +77,14 @@ public class AutomatedBandDemo
 		bass.setContext(bassContext);
 		bass.setSwingRatio(1.3f);
 		band.addComposer(bass);
-		
-		KeyboardComposer piano = new KeyboardComposer("Piano", 0, 2); // acoustic grand piano
-		
-		TonalComposer hand = piano.getLeftHandComposer();
+	}
+	
+	protected void addKeyboardComposer(int program, int channel) {
+		TonalComposer leftHandComposer = new TonalComposer("Keyboard Left", program, channel);
+		band.addComposer(leftHandComposer);
+		TonalComposer rightHandComposer = new TonalComposer("Keyboard Right", program, channel);
+		band.addComposer(rightHandComposer);
+
 		TonalComposer.Context pianoContext = new TonalComposer.Context();
 		pianoContext.setRepeatPitchProbability(0.25f);
 		pianoContext.setMaxPitchChange(3); // odd numbers work best for tertian intervals
@@ -71,10 +94,9 @@ public class AutomatedBandDemo
 		pianoContext.setLegato(0.9f);
 		pianoContext.setMinPitch(42);
 		pianoContext.setMaxPitch(56);
-		hand.setContext(pianoContext);
-		hand.setSwingRatio(1.3f);
+		leftHandComposer.setContext(pianoContext);
+		leftHandComposer.setSwingRatio(1.3f);
 		
-		hand = piano.getRightHandComposer();
 		pianoContext = new TonalComposer.Context();
 		pianoContext.setMelodyProbability(0.75f); // melodic probability
 		pianoContext.setRepeatPitchProbability(0.25f);
@@ -85,11 +107,10 @@ public class AutomatedBandDemo
 		pianoContext.setLegato(0.9f);
 		pianoContext.setMinPitch(62); // slight overlap with left hand 9ths
 		pianoContext.setMaxPitch(78);
-		hand.setContext(pianoContext);
-		hand.setSwingRatio(1.3f);
-		band.addComposer(piano);
+		rightHandComposer.setContext(pianoContext);
+		rightHandComposer.setSwingRatio(1.3f);
 	}
-
+	
 	/**
 	 * Compose nbars of 4/4 music to the specified Standard MIDI File.
 	 * @param nbars the number of bars of 4/4 to compose
